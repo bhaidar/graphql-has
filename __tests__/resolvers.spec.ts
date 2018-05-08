fb6b469b01e60c9062b833d73d1dec7b370a6adb
Marcello Barile <marcello.barile@gmail.com>
2018-05-08 15:59:40 +0200

    added dummy example test

--- __tests__/resolvers.spec.ts
+++ __tests__/resolvers.spec.ts
@@ -4,3 +4,7 @@ import { BaseConnector as MockBaseConnector } from '../server/connectors/rest/__
 // Mocked base service
 import { BaseConnector } from '../server/connectors/rest/base';
 jest.mock('../server/connectors/rest/base');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(1+2).toBe(3);
+});
